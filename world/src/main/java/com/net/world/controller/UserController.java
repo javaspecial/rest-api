ea52a0e91f1b3357b87f8ad13e335fe37b434a02
javaspecial <shadathcse@gmail.com>
2019-07-03 02:20:44 -0700

    Exception Handling and Response status added

--- world/src/main/java/com/net/world/controller/UserController.java
+++ world/src/main/java/com/net/world/controller/UserController.java
@@ -7,9 +7,9 @@ package com.net.world.controller;
 
 import com.net.world.model.User;
 import com.net.world.service.UserService;
-import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,29 +32,49 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/listOfUser")
-    public List<User> listOfUser() {
-        return userService.getUserList();
+    public ResponseEntity<?> listOfUser() {
+        try {
+            return new ResponseEntity(userService.getUserList(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping(value = "/add")
-    public List<User> addUser(@RequestBody User user) {
-        userService.save(user);
-        return userService.getUserList();
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try {
+            userService.save(user);
+            return new ResponseEntity(userService.getUserList(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/updateUser/{userId}")
-    public User updateUser(@PathVariable(value = "userId") Integer userId, @Valid @RequestBody User user) {
-        return userService.update(userId, user);
+    public ResponseEntity<?> updateUser(@PathVariable(value = "userId") Integer userId, @Valid @RequestBody User user) {
+        try {
+            return new ResponseEntity(userService.update(userId, user), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/deleteUser/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable(value = "userId") Integer userId) {
-        return userService.delete(userId);
+        try {
+            return userService.delete(userId);
+        } catch (Exception ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/deleteOneByUserId/{userEmail}")
     public ResponseEntity<String> deleteOneByUserId(@PathVariable(value = "userEmail") String userEmail) {
-        return userService.deleteOneByUserEmail(userEmail);
+        try {
+            return userService.deleteOneByUserEmail(userEmail);
+        } catch (Exception ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
