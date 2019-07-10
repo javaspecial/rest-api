df43e3a0f25e6b2dae0fff871fc365f65e50663a
javaspecial <shadathcse@gmail.com>
2019-07-10 03:56:55 +0100

    spring security removed

--- world/src/main/java/com/net/world/WorldApplication.java
+++ world/src/main/java/com/net/world/WorldApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.net.world.controller", "com.net.world.serviceImpl", "com.neti.world.aspect", "com.net.world.security"})
+@ComponentScan({"com.net.world.controller", "com.net.world.serviceImpl", "com.neti.world.aspect"})
 @EntityScan("com.net.world.model")
 public class WorldApplication {
 
